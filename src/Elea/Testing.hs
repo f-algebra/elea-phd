241de34bd6ecba2083702a8e0e4c548b601f9e6c
wsonnex <will@sonnex.name>
2013-04-18 21:26:58 +0200

    Got rid of some old commented out code.

--- src/Elea/Testing.hs
+++ src/Elea/Testing.hs
@@ -48,31 +48,3 @@ loadPrelude = Parse.program prelude
 
 term :: String -> Elea Term
 term = Error.check Typing.check . Parse.term
-
-{-
-  
-run :: HUnit.Testable t => Zeno t -> Test
-run = HUnit.test . flip evalState empty
-
-term :: String -> Zeno ZTerm
-term = ZML.readTerm
-
-assertAlphaEq :: ZTerm -> ZTerm -> Test
-assertAlphaEq x y = 
-  label (show (Logic.Equal x y))
-  $ assert (x `alphaEq` y)
-
-newVar :: String -> String -> Zeno ZVar
-newVar name_s typ_s = do
-  typ <- ZML.readType typ_s
-  new_var <- Var.declare name_s typ Var.Universal
-  Zeno.defineTerm name_s (Term.Var new_var)
-  return new_var
-  
--- Move this into the counter example finding module
--- it's a assertion that will dynamically check equality
-assertEq :: ZTerm -> ZTerm -> a -> a
-assertEq t1 t2 x = x
-
- -}
-
