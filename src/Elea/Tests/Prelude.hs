2ccafe0c7d444d7cb7fbf1efa67f0ab4cbd40477
Will Sonnex <will@sonnex.name>
2015-12-31 23:53:54 +0000

    Revamped Prelude tests

--- src/Elea/Tests/Prelude.hs
+++ src/Elea/Tests/Prelude.hs
@@ -5,36 +5,16 @@ module Elea.Tests.Prelude
 where
 
 import Elea.Prelude hiding ( assert )
-import Test.HUnit ( Test (..), assert )
+import Test.HUnit ( Test (..) )
+import qualified Test.HUnit as HUnit
 import qualified Data.Set as Set
 
-tests = TestLabel "Prelude"
-  $ TestList 
-  [ test_deleteIndices
-  , test_takeIndices
-  , test_isNub
-  , test_nubOrd
-  , test_removeAll ]
-  
-testCase = TestCase . assert
-testList = TestList . map testCase
+tests :: Test
+tests = TestLabel "Prelude" testPrelude
 
-test_deleteIndices = testCase
-  $ deleteIndices [1, 3, 6] [0..7] == [0, 2, 4, 5, 7]
-  
-test_takeIndices = testCase
-  $ takeIndices [1, 3, 6] [0..7] == [1, 3, 6]
-  
-test_isNub = testList $ 
-  [ isNub [0, 2, 1]
-  , not $ isNub [3, 1, 2, 1, 5] 
-  , isNub ([] :: [Int]) ]
-  
-test_nubOrd = testList $
-  [ nubOrd ([] :: [Int]) == []
-  , nubOrd [4, 7, 2, 4, 5, 7, 1] == [4, 7, 2, 5, 1] ]
-  
-test_removeAll = testList $
-  [ removeAll (Set.fromList [1, 3]) [0..5] == [0, 2, 4, 5]
-  , removeAll (Set.fromList [0..9]) [1..10] == [] ]
-
+testPrelude :: Test
+testPrelude = TestCase $ do
+  HUnit.assertEqual "delete indices" [0, 2, 4, 5, 7] (deleteIndices [1, 3, 6] [0..7])
+  HUnit.assertEqual "take indices" [1, 3, 6] (takeIndices [1, 3, 6] [0..7])
+  HUnit.assertEqual "removeAll 1" [0, 2, 4, 5] (removeAll (Set.fromList [1, 3]) [0..5])
+  HUnit.assertEqual "removeAll 2" [] (removeAll (Set.fromList [0..9]) [1..10])
